0a5c988429beea50975c7c2fedfdf8422356897c
Nandinih18 <nandinih18.com>
2019-12-30 08:55:15 +0530

    blah

---
+++ EmployeeUi.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class EmployeeUi {
+	public static void main(String[]args) {
+		EmployeeUi ui=new EmployeeUi();
+		ui.execute();
+	}
+	
+	Map<Integer,Employee> store=new HashMap<>();
+	
+	public void execute() {
+		Employee e1=new Employee(1,"abhinav");
+		store.put(1, e1);
+		Manager m1=new Manager(2,"chandra");
+		store.put(2,m1);
+		m1.addManagedEmployee(e1);
+		print();
+		Employee fetched=findById(2);
+		System.out.println(fetched.getName());
+	}
+	
+	public Employee findById(int id) {
+		Employee e=store.get(id);
+		return e;
+	}
+
+	
+	public void print() {
+		Set<Integer> keys=store.keySet();
+		for (Integer id: keys) {
+			Employee e = store.get(id);
+			System.out.println(e.getName() + e.getBalance());
+			boolean isManager=e instanceof Manager;
+			if(isManager) {
+				Manager m=(Manager) e;
+				System.out.println("managed employees");
+				Employee[] managedEmployees = m.getManagedEmployees();
+				for(Employee managed: managedEmployees) {
+					if(managed != null) {
+						System.out.println(managed.getName()+" "+managed.getBalance());
+					}
+				}
+			}
+		}
+	}
+}
